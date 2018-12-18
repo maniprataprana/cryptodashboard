22ba545514016f421cbe5f6cbaa267844c24ff32
Rana Mani Pratap <rapratap@cisco.com>
2018-12-18 20:59:13 +0530

    Added AppBar and grid

--- src/App/index.js
+++ src/App/index.js
@@ -3,13 +3,13 @@ import WelcomeMessage from "./WelcomeMessage";
 import "./App.css";
 import styled, { css } from "styled-components";
 import AppLayout from "./AppLayout";
-
-
+import AppBar from "./AppBar";
 
 class App extends Component {
   render() {
     return (
       <AppLayout>
+        <AppBar />
         <WelcomeMessage />
       </AppLayout>
     );
