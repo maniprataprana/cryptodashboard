84a71a06fd5ebfbded2d3e7b3870a1a35cebb234
Rana Mani Pratap <rapratap@cisco.com>
2018-12-22 22:18:33 +0530

    added search

---
+++ src/Settings/Search.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+import { backgroundColor2, fontSize2 } from "../Shared/Styles";
+
+const SearchGrid = styled.div`
+  display: grid;
+  grid-template-columns: 200px 1fr;
+`;
+
+const SearchInput = styled.input`
+    ${backgroundColor2}
+    ${fontSize2}
+    color: #1163c9;
+    height: 25px;
+    border: 1px solid;
+    place-self: center left;
+
+`;
+
+const Search = () => <SearchGrid><h2>Search all coins</h2><SearchInput /></SearchGrid>;
+
+export default Search;
--- src/Settings/index.js
+++ src/Settings/index.js
@@ -3,11 +3,15 @@ import React from "react";
 import WelcomeMessage from "./WelcomeMessage";
 import ConfirmButton from "./ConfirmButton";
 import CoinGrid from "./CoinGrid";
-
 import Page from "../Shared/Page";
+import Search from "./Search";
 
 export default () => (
   <Page name="settings">
-        <WelcomeMessage /> <CoinGrid topSection /> <ConfirmButton /> <CoinGrid />
+    <WelcomeMessage />
+    <CoinGrid topSection />
+    <ConfirmButton />
+    <Search />
+    <CoinGrid />
   </Page>
 );
